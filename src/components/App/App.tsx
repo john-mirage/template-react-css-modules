fc1582669f90173650707537c1af97f3dad65fe4
john-mirage <chaosdynamix@posteo.net>
2022-10-21 04:49:57 +0200

    feat: test app component with css module

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,11 @@
+import classes from "./App.module.css";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className={classes.app}>
+      <div className={classes.appTitle}></div>
+    </div>
+  );
 };
 
 export default App;
